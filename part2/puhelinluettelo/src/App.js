85ffc33d402cae4d5fbfeac452f73224f309a96f
lauri-ra <lrautiala+1@gmail.com>
2022-04-12 00:09:09 +0300

    Puhelinluettelo step1

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,24 +1,58 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
 
+
+  const addName = (event) => {
+    event.preventDefault()
+
+    const nameObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const Person = ({ person }) => {
+    return (
+      <p>{person.name}</p>
+    )
+  }
+  
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input 
+          value={newName} 
+          onChange={handleNameChange}/>
         </div>
+        <div>debug: {newName}</div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
-    </div>
-  );
-};
 
-export default App;
+      <div>
+        {persons.map(person => 
+          <Person key={person.name} person={person} />
+        )}
+      </div>
+    </div>
+  )
+
+}
+
+export default App
