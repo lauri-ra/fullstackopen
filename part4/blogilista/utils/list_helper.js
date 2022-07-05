267d7661bb62597038f3fe1e3769ec3dc82d2da5
lauri-ra <lrautiala+1@gmail.com>
2022-07-05 14:04:32 +0300

    Task 4.4

--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const sumOfLikes = blogs.reduce(
+        (total, item) => total + item.likes, 0
+    )
+    
+    return sumOfLikes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
