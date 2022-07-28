9eb877b5cabcec3def7553992395e033d4a8f44e
lauri-ra <lrautiala+1@gmail.com>
2022-07-28 23:52:05 +0300

    Task 4.19

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,6 +1,17 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const auth = request.get('authorization')
+
+  if(auth && auth.toLowerCase().startsWith('bearer ')) {
+    return auth.substring(7)
+  }
+
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -31,7 +42,16 @@ blogsRouter.post('/', async (request, response) => {
     body.likes = 0
   }
 
-  const user = await User.findById(body.userId)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  if(!token || !decodedToken.id) {
+    return response.status(401).json({
+      error: 'Token missing or invalid'
+    })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
