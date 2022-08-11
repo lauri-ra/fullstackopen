9bee0322f22d97e34a6f25cb696252ed85ce79f5
lauri-ra <lrautiala+1@gmail.com>
2022-08-11 23:42:45 +0300

    Task 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,46 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleVisibility = () => {
+    setShowDetails(!showDetails)
+}
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const simpleBlog = (
+    <div>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisibility}>view</button>
+    </div>
+  )
+
+  const detailedBlog = (
+    <div>
+      <div>
+        <div>{blog.title} {blog.author}</div>
+        <div>{blog.url}</div>
+        <div>
+          likes: {blog.likes}
+          <button>like</button>
+        </div>
+        <button onClick={toggleVisibility}>hide</button>
+      </div>
+    </div>  
+  )
+
+  return (
+    <div style={blogStyle}>
+      {showDetails ? detailedBlog : simpleBlog}
+    </div>
+  ) 
+}
 
 export default Blog
