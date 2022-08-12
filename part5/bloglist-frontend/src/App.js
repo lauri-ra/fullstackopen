7db83e8060ad61e7cecc8d7c36f1714f15541dde
lauri-ra <lrautiala+1@gmail.com>
2022-08-12 15:41:35 +0300

    Task 5.9, sort by likes

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -22,7 +22,7 @@ const App = () => {
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    )
   }, [])
 
   useEffect(() => {
@@ -148,9 +148,12 @@ const App = () => {
         <BlogForm createBlog={createBlog}/>
       </Togglable>
 
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
-      )}
+      {blogs
+        .sort((a, b) =>  b.likes-a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} updateBlog={updateBlog}/>
+        )
+      }
     </div>
   )
 
