e2661ff66c4e51e11925f58cde373d66b1fec8e1
lauri-ra <lrautiala+1@gmail.com>
2022-08-23 17:07:38 +0300

    Task 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,34 +1,56 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import { exact } from 'prop-types'
 
-test('renders content', () => {
-    const blog = {
-        title: 'test title',
-        author: 'test author',
-        likes: '1',
-        url: 'google.com'
-    }
+describe('Blog content', () => {
+    beforeEach(() => {
+        const blog = {
+            title: 'test title',
+            author: 'test author',
+            likes: 714,
+            url: 'google.com'
+        }
 
-    const mockUpdate = jest.fn()
-    const mockRemove = jest.fn()
+        const mockUpdate = jest.fn()
+        const mockRemove = jest.fn()
 
-    render(<Blog blog={blog} updateBlog={mockUpdate} removeBlog={mockRemove}/>)
+        render(<Blog blog={blog} updateBlog={mockUpdate} removeBlog={mockRemove}/>)
+    })
 
-    const title = screen.getByText(
-        'test title', { exact: false }
-    )
-    expect(title).toBeDefined()
+    test('renders collapsed content', () => {
+        const title = screen.getByText(
+            'test title', { exact: false }
+        )
+        expect(title).toBeDefined()
 
-    const author = screen.getByText(
-        'test author', { exact: false }
-    )
-    expect(author).toBeDefined()
+        const author = screen.getByText(
+            'test author', { exact: false }
+        )
+        expect(author).toBeDefined()
 
-    const likes = screen.queryByText('1')
-    expect(likes).toBeNull()
+        const likes = screen.queryByText('714')
+        expect(likes).toBeNull()
 
-    const url = screen.queryByText('google.com')
-    expect(url).toBeNull()
+        const url = screen.queryByText('google.com')
+        expect(url).toBeNull()
+    })
+
+    test('renders extended content', async () => {
+        const user = userEvent.setup()
+        const button = screen.getByText('view')
+        await user.click(button)
+
+        const likes = screen.getByText(
+            '714', { exact: false }
+        )
+        expect(likes).toBeDefined()
+
+        const url = screen.getByText(
+            'google.com', { exact: false }
+        )
+        expect(url).toBeDefined()
+    })
 })
