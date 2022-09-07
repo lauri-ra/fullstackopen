955311fef1a466e3832a3bc1ce52153261dd0ff8
lauri-ra <lrautiala+1@gmail.com>
2022-09-07 18:29:11 +0300

    Fix to task 5.19

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,5 +1,5 @@
 describe('Blog ', function() {
-    beforeEach('Blog app', function() {
+    beforeEach(function() {
         cy.request('POST', 'http://localhost:3003/api/testing/reset')
         // create user here
         const user = {
@@ -13,13 +13,11 @@ describe('Blog ', function() {
     })
 
     it('login form is shown', function() {
-      cy.visit('http://localhost:3000')
       cy.contains('Login to application')
     })
 
     describe('Login tests', function() {
       it('logging in with incorrect credentials fails', function() {
-        cy.visit('http://localhost:3000')
         cy.get('#username').type('test')
         cy.get('#password').type('test')
 
@@ -28,7 +26,6 @@ describe('Blog ', function() {
       })
 
       it('user can log in with correct credentials', function() {
-        cy.visit('http://localhost:3000')
         cy.get('#username').type('root')
         cy.get('#password').type('secret')
         cy.get('#login-button').click()
@@ -36,23 +33,20 @@ describe('Blog ', function() {
     })
 
     describe('When logged in', function() {
-/*       beforeEach(function() {
-        cy.visit('http://localhost:3000')
-        cy.get('#username').type('root')
-        cy.get('#password').type('secret')
-        cy.get('#login-button').click()
-      }) */
+        beforeEach(function() {
+          cy.login({ username: 'root', password: 'secret' })
+        })
 
-      it('a blog can be created', function() {
-        cy.contains('New blog').click()
+        it('a blog can be created and is visible on the list', function() {
+          cy.contains('New blog').click()
 
-        cy.get('#title').type('test title')
-        cy.get('#author').type('test author')
-        cy.get('#url').type('test url')
+          cy.get('#title').type('test title')
+          cy.get('#author').type('test author')
+          cy.get('#url').type('test url')
+          cy.get('#create-button').click()
 
-        cy.get('#create-button').click()
-
-
+          cy.visit('http://localhost:3000')
+          cy.contains('test title test author')
       })
     })
   })
