bdf7383848ded9e457f27fd60e349d5de7486130
lauri-ra <lrautiala+1@gmail.com>
2022-10-03 14:33:23 +0300

    Task 6.15

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -28,5 +29,12 @@ const anecdoteSlice = createSlice({
   }
 })
 
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+ 
 export const { setAnecdotes, createAnecdote, vote } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
