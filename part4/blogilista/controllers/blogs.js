4634ba435314010b46f6d4d6c2344280f39265e5
lauri-ra <lrautiala+1@gmail.com>
2022-10-17 23:58:45 +0300

    Put request fix to backend

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,78 +1,74 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
-const { userExtractor } = require('../utils/middleware')
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+const { userExtractor } = require("../utils/middleware");
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog
-    .find({}).populate('user', { username: 1, name: 1})
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
 
-  response.json(blogs)
-})
+  response.json(blogs);
+});
 
-blogsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
 
-  if(blog) {
-    response.json(blog.toJSON())
+  if (blog) {
+    response.json(blog.toJSON());
+  } else {
+    response.status(404).end();
   }
-  else {
-    response.status(404).end()
-  }
-})
-  
-blogsRouter.post('/', userExtractor, async (request, response) => {
-  const body = request.body
+});
 
-  if(!body.title || !body.url) {
-    return response.status(400).json({ error: 'Blog body missing content' })
+blogsRouter.post("/", userExtractor, async (request, response) => {
+  const body = request.body;
+
+  if (!body.title || !body.url) {
+    return response.status(400).json({ error: "Blog body missing content" });
   }
 
-  if(body.likes === null) {
-    body.likes = 0
+  if (body.likes === null) {
+    body.likes = 0;
   }
 
-  const user = request.user
+  const user = request.user;
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: user._id
-  })
+    user: user._id,
+  });
 
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
 
-  response.status(201).json(savedBlog)
-})
+  response.status(201).json(savedBlog);
+});
 
-blogsRouter.delete('/:id', userExtractor, async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
-  const user = request.user
+blogsRouter.delete("/:id", userExtractor, async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  const user = request.user;
 
-  if(user.id.toString() === blog.user.toString()) {
-    await blog.remove()
-    response.status(204).end()
+  if (user.id.toString() === blog.user.toString()) {
+    await blog.remove();
+    response.status(204).end();
+  } else {
+    response.status(401).end();
   }
-  else {
-    response.status(401).end()
-  }
-})
+});
 
-blogsRouter.put('/:id', async (request, response) => {
-  const body = request.body
+blogsRouter.put("/:id", async (request, response) => {
+  const blogToUpdate = new Blog(request.body).toJSON();
 
-  const blog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  }
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    blogToUpdate,
+    {
+      new: true,
+    }
+  ).populate("user", { username: 1, name: 1 });
+  response.json(updatedBlog);
+});
 
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-  response.json(updatedBlog)
-})
-
-module.exports = blogsRouter
+module.exports = blogsRouter;
--- part4/blogilista/utils/middleware.js
+++ part4/blogilista/utils/middleware.js
@@ -1,65 +1,62 @@
-const logger = require('./logger')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
+const logger = require("./logger");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 const requestLogger = (request, response, next) => {
-    logger.info('Method:', request.method)
-    logger.info('Path:  ', request.path)
-    logger.info('Body:  ', request.body)
-    logger.info('---')
-    next()
-}
+  logger.info("Method:", request.method);
+  logger.info("Path:  ", request.path);
+  logger.info("Body:  ", request.body);
+  logger.info("---");
+  next();
+};
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 const errorHandler = (error, request, response, next) => {
-    logger.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    }
-    else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-    else if (error.name === 'JsonWebTokenError') {
-        return response.status(401).json({ error: 'invalid token' })
-    }
+  logger.error(error.message);
 
-    next(error)
-}
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({ error: "invalid token" });
+  }
+
+  next(error);
+};
 
 const tokenExtractor = (request, response, next) => {
-    const auth = request.get('authorization')
+  const auth = request.get("authorization");
 
-    if(auth && auth.toLowerCase().startsWith('bearer ')) {
-      request.token = auth.substring(7)
-    }
+  if (auth && auth.toLowerCase().startsWith("bearer ")) {
+    request.token = auth.substring(7);
+  }
 
-    next()
-}
+  next();
+};
 
 const userExtractor = async (request, response, next) => {
-    if(request.token) { 
-        const decodedToken = jwt.verify(request.token, process.env.SECRET)
-        const user = await User.findById(decodedToken.id)
+  if (request.token) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    const user = await User.findById(decodedToken.id);
 
-        request.user = user
-    }
-    else {
-        return response.status(401).json({
-            error: 'Token missing or invalid'
-        })
-    }
+    request.user = user;
+  } else {
+    return response.status(401).json({
+      error: "Token missing or invalid",
+    });
+  }
 
-    next()
-}
-  
+  next();
+};
+
 module.exports = {
-    requestLogger,
-    unknownEndpoint,
-    errorHandler,
-    tokenExtractor,
-    userExtractor
-}
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+  tokenExtractor,
+  userExtractor,
+};
