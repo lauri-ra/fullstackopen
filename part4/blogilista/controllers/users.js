a0edc483ee6cbd1a596e7dd0911d92bd02d94d7f
lauri-ra <lrautiala+1@gmail.com>
2022-11-04 15:06:30 +0200

    Fixed missing endpoint

--- part4/blogilista/controllers/users.js
+++ part4/blogilista/controllers/users.js
@@ -1,43 +1,52 @@
-const bcrypt = require('bcrypt')
-const usersRouter = require('express').Router()
-const User = require('../models/user')
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
 
-usersRouter.get('/', async (request, response) => {
-    const users = await User
-        .find({}).populate('blogs', { title: 1, author: 1})
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({}).populate("blogs", { title: 1, author: 1 });
 
-    response.json(users)
-})
+  response.json(users);
+});
 
-usersRouter.post('/', async (request, response) => {
-    const { username, name, password } = request.body
+usersRouter.get("/:id", async (request, response) => {
+  const user = await User.findById(request.params.id);
 
-    if(username.length < 3 || password.length < 3) {
-        return response.status(400).json({
-            error: 'Username and/or password too short'
-        })
-    }
-    
-    const exsistingUsr = await User.findOne({ username })
+  if (user) {
+    response.json(user.toJSON());
+  } else {
+    response.status(404).end();
+  }
+});
 
-    if(exsistingUsr) {
-        return response.status(400).json({
-            error: 'Username needs to be unique'
-        })
-    }
+usersRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body;
 
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
+  if (username.length < 3 || password.length < 3) {
+    return response.status(400).json({
+      error: "Username and/or password too short",
+    });
+  }
 
-    const user = new User({
-        username,
-        name,
-        passwordHash,
-    })
+  const exsistingUsr = await User.findOne({ username });
 
-    const savedUser = await user.save()
+  if (exsistingUsr) {
+    return response.status(400).json({
+      error: "Username needs to be unique",
+    });
+  }
 
-    response.status(201).json(savedUser)
-})
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
 
-module.exports = usersRouter
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  response.status(201).json(savedUser);
+});
+
+module.exports = usersRouter;
