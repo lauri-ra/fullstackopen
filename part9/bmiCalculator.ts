a824ab5c525e110d97f96f771e441077380570c2
lauri-ra <lrautiala+1@gmail.com>
2022-11-24 22:22:59 +0200

    Task 9.1

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,19 @@
+const calculateBmi = (height: number, weight: number): string => {
+    const result  = weight / Math.pow(height / 100, 2);
+
+    if(result < 18.5) {
+        return 'Underweight';
+    } 
+    else if(result >= 18.5 && result < 25) {
+        return 'Normal (healthy weight)';
+    }
+    else if(result >= 25 && result < 30) {
+        return 'Overweight (Pre-obese)';
+    }
+    else if(result > 30) {
+        return 'Obese';
+    }
+}
+
+console.log(calculateBmi(165, 68));
+console.log(calculateBmi(180, 74));
